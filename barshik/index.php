ea45ac4adccd2ed1056e3f08321b547af15f01e8
Natalua9 <148848234+Natalua9@users.noreply.github.com>
2024-04-05 11:38:11 +0500

    неполная верстка главной страницы

---
+++ barshik/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Главная</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="naw-header">
+                <img src="images\Group 8192.png" alt="" class="logo">
+                <h1>БарШик</h1>
+                <div class="naw-menu">
+                    <a href="/">Главная</a>
+                    <a href="">Каталог</a>
+                    <a href="#footer">Контакты</a>
+                    <a href="">Войти</a>
+                </div>
+            </div>
+        </div>
+    </header>
+<main>
+    <div class="catalog">
+            <h2>Каталог</h2>
+            <div class="category">
+                <button class="button-category">Соки</button>
+                <button class="button-category">Кофе</button>
+                <button class="button-category">Газированные напитки</button>
+                <button class="button-category">Молочные напитки</button>
+                <button class="button-category">Вода</button>
+                <button class="button-category">Детские напитки</button>
+            </div>
+            <div class="bloc-drinks">
+                
+                <div class="drink">
+                            <div class="tovar">
+                        <img src="images\af7e75ccce5f72ea2a617988af31786b.jpeg" alt="">
+                        <h4>Латте</h3>
+                        <button class=" button-tovar">Добавить в корзину</button>
+                            </div>
+                            <div class="tovar">
+                    <img src="images\31762_530x300x85_c.webp" alt="">
+                    <h4>Вода</h3>
+                        <button class=" button-tovar">Добавить в корзину</button>
+                            </div>
+                            <div class="tovar">
+                    <img src="images\51498_530x300x85_c.webp" alt="">
+                    <h4>Сок</h3>
+                        <button class=" button-tovar">Добавить в корзину</button>
+                            </div>
+                    </div>
+                
+            </div>
+            <button class="podrobnee">Подробнее</button>
+    </div>
+<!-- часть с текстом -->
+<div class="description">
+<div class="text-discription">
+    <h4>Закажите напитки к себе домой</h4>
+</div>
+<div class="bloc-img-description">
+    <img src="images\Group 8192.png" alt="" class="logo">
+    <img src="images\Group 8195.png" alt="" class="img-description">
+</div>
+</div>
+
+
+</main>
+    <!-- подвал -->
+<footer id="footer">
+    <div class="container">
+        <div class="connection">
+            <h4>Связь с нами</h4> 
+            <div class="images-connection">
+                <img src="images\icons8-instagram-48.png" alt="" srcset="">
+                <img src="images\icons8-vk-com-48.png" alt="" srcset="">
+                <img src="images\iconfinder-social-media-applications-23whatsapp-4102606_113811.png" class="icon-whatsapp">
+            
+            </div>
+          
+        </div>
+        <div class="clock-work">
+                <h4>Часы  работы:</h4>
+                <p>10:00 - 23:00</p>
+            </div>
+        <!-- <hr>
+        <p class="copirater">© 2023 Копирование запрещено. Все права защищены.</p> -->
+    </div>
+</footer>
+</body>
+</html>
